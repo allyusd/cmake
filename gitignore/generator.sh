2c99e3cbcefc5905c18a06f2e9e9f7c219189fd7
allyusd <allyusd@yahoo.com.tw>
2016-10-23 17:19:14 +0800

    add CMake.gitignore

--- gitignore/generator.sh
+++ gitignore/generator.sh
@@ -2,4 +2,4 @@ rm -r -f gitignore/
 
 git clone --depth 1 https://github.com/github/gitignore.git
 
-cat Project.gitignore gitignore/VisualStudio.gitignore gitignore/C++.gitignore gitignore/Qt.gitignore > ../.gitignore
+cat Project.gitignore gitignore/VisualStudio.gitignore gitignore/C++.gitignore gitignore/Qt.gitignore gitignore/CMake.gitignore > ../.gitignore
